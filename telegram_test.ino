15ef80eac24aec3c10f7e009708e1efbe35e07e7
snkinog36 <81772032+snkinog36@users.noreply.github.com>
2023-03-10 09:21:48 -0800

    Add files via upload

---
+++ telegram_test.ino
@@ -0,0 +1,58 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/telegram-esp32-motion-detection-arduino/
+  
+  Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+*/
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+// Replace with your network credentials
+const char* ssid = "iPhone (145)";
+const char* password = "deniavdija";
+
+// Initialize Telegram BOT
+#define BOTtoken "5923616599:AAEpZyI7FhS2_O7pf2MB45kqasv9dgBUjDQ"  // your Bot Token (Get from Botfather)
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "5428465157"
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // Attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  bot.sendMessage(CHAT_ID, "Bot started up", "");
+}
+
+void loop() {
+  
+  bot.sendMessage(CHAT_ID, "Motion detected!!", "");
+  Serial.println("Motion Detected");
+  delay(1000);
+}
